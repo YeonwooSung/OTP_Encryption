01acb52016b7fe08e2c14a3ac5727f08cc63a0dc
Yeonwoo Sung <30489717+YeonwooSung@users.noreply.github.com>
2018-04-18 07:47:20 +0100

    Checking the option string

--- my_otp.c
+++ my_otp.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <sys/types.h> /* pid_t */
+#include <stdlib.h>
 #include <unistd.h>
+
+void runProcesses() {
+    for (int kid = 0; kid < 2; kid++) {
+        //
+    }
+}
 
 int main(int argc, char *argv[]) {
     //Flags for the options.
     char iFlag = 0; //flag for the -i option
     char oFlag = 0; //flag for the -o option
     char kFlag = 0; //flag for the -k option
-
-    FILE *input = stdin;
-    FILE *output = stdout;
 
     int opt; //to store the result of the getopt function.
 
@@ -18,7 +23,7 @@ int main(int argc, char *argv[]) {
                 if (kFlag) { //check if the user input multiple -k option
                     printf("Only one 'k' option is required!");
                     printf("usage: my_otp [-i infile] [-o outfile] -k keyfile");
-                    return 0; //terminate the program
+                    exit(1); //terminate the program
                 }
                 kFlag = 1;
                 break;
@@ -26,7 +31,7 @@ int main(int argc, char *argv[]) {
                 if (iFlag) { //check if the user input more than 1 -i option
                     printf("Only one 'i' option!");
                     printf("usage: my_otp [-i infile] [-o outfile] -k keyfile");
-                    return 0; //terminate the program
+                    exit(1); //terminate the program
                 }
                 iFlag = 1; //change the value of the iFlag to 1 to change the stream input from stdin to file stream.
                 break;
@@ -34,7 +39,7 @@ int main(int argc, char *argv[]) {
                 if (oFlag) { //check if the user input more than 1 -i option
                     printf("Only one 'o' option!");
                     printf("usage: my_otp [-i infile] [-o outfile] -k keyfile");
-                    return 0; //terminate the program
+                    exit(1); //terminate the program
                 }
                 oFlag = 1;
                 break;
@@ -45,7 +50,10 @@ int main(int argc, char *argv[]) {
 
     if (kFlag == 0) {
         printf("usage: my_otp [-i infile] [-o outfile] -k keyfile");
-        return 0;
+        exit(1); //terminate the program
     }
+
+    runProcesses();
+
     return 0;
 }
