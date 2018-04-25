a5d3098c28ca4d15062e825a2d9ff4550ab5a7e1
Yeonwoo Sung <30489717+YeonwooSung@users.noreply.github.com>
2018-04-25 21:00:45 +0900

    Change some details

--- multi-threaded/syncMessage.c
+++ multi-threaded/syncMessage.c
@@ -6,25 +6,41 @@
 #include "msg_queue.h"
 
 // Gets the input message from the stdin stream.
-void getInputsToStream(MQueue *q) {
+int getInputsToStream(MQueue *q) {
 
     char *buffer = (char *) malloc(256);
     char *savePoint = buffer; //will use this to free the allocated memory
 
-    fgets(buffer, 256, stdin);
+    int totalNumOfChars = 0;
 
-    int counter = 0;
+    while (fgets(buffer, 256, stdin) != NULL) {
+        int counter = 0;
 
-    while (*buffer != '\n' && *buffer != '\0' && counter < 256) {
-        //
-        int input = *buffer;
-        send_msg(q, input);
-        buffer += 1;
-        counter += 1;
+        while (*buffer != '\n' && *buffer != '\0' && counter < 256) {
+            //
+            int input = *buffer;
+            send_msg(q, input);
+            buffer += 1;
+            counter += 1;
+            totalNumOfChars += 1;
+        }
+
+        if (*buffer == '\n') { //check if the current character is the next line character.
+            int input = *buffer;
+            send_msg(q, input);
+            totalNumOfChars += 1;
+        }
+
+        free(savePoint); //free the allocated memory
+
+        buffer = (char *) malloc(256);
+
+        savePoint = buffer;
     }
 
-    free(savePoint); //free the allocated memory
+    free(savePoint);
 
+    return totalNumOfChars;
 }
 
 // Prints the output message through the stdout stream.
@@ -35,43 +51,31 @@ void *writeOutputsToStream(void *queue) {
 }
 
 // The main function.
-int main(int argc, char *argv[]) {
-
-    int doJoin;
-
-    if (argc > 1 && strcmp(argv[1], "join") == 0) { //check the command line argument
-        doJoin = 1; //set the value of doJoin as 1.
-    } else {
-        //Otherwise, set the value of doJoin as 0.
-        doJoin = 0;
-    }
-
-    pthread_t writer[10];
+int main(void) {
 
     MQueue *queue = (MQueue *) malloc(sizeof(MQueue));
     initMQueue(queue);
 
-    getInputsToStream(queue); //get the user input
+    int num = getInputsToStream(queue); //get the user input
+
+    pthread_t writer[num];
 
     int i, err;
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < num; i++) {
         err = pthread_create(&writer[i], NULL, writeOutputsToStream, (void *)queue); //create the new thread that writes output messages.
         if (err != 0) {
             fprintf(stderr, "Failed to make a thread\n");
             exit(1);
         }
-        //printf("Finish the %02d turn\n", i);
     }
 
     nanosleep((const struct timespec[]){{0, 50000L}}, NULL); //use the nanosleep to sleep the main thread
 
     printf("sleep finish\n");
 
-    if (doJoin) {
-        int j, status;
-        for (j = 0; j < 10; j++) {
-            pthread_join(writer[j], (void *)&status);
-        }
+    int j, status;
+    for (j = 0; j < num; j++) {
+        pthread_join(writer[j], (void *)&status);
     }
 
     freeMQueue(queue);
